38d61d507b0afea96c2b7713cca0955219c93abc
Walter Diaz <walter13457@gmail.com>
2022-10-11 12:22:37 -0600

    node

---
+++ cod/EjercicioNode1/Main.js
@@ -0,0 +1,9 @@
+const{suma,rest} = require("./Operations");
+
+const a=8;
+const b=10;
+
+const suma1=suma(a,b);
+const resta= rest(a,b);
+console.log(suma1);
+console.log(resta);
---
+++ cod/EjercicioNode1/Operations.js
@@ -0,0 +1,9 @@
+function suma(n1,n2){
+    return n1+n2;
+    }
+    
+    function rest(n1,n2){
+        return n1-n2;
+    }
+    
+    module.exports ={suma,rest};
